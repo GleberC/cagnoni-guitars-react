3a53dc5a88fdf2061f3372c3e80b0ff3b792346c
GleberC <gleber10@yahoo.com.br>
2025-05-20 20:50:04 -0300

    imagens na pasta assets

--- src/components/Gallery/data.js
+++ src/components/Gallery/data.js
@@ -1,147 +1,53 @@
-const BASE_CONSTRUCOES = '/cagnoni-guitars-react/images/construcoes/';
-const BASE_CUSTOMIZACOES = '/cagnoni-guitars-react/images/customizacoes/';
-const BASE_MANUTENCOES = '/cagnoni-guitars-react/images/manutencoes/';
+import B01 from '../../assets/images/construcoes/B01.jpg';
+import B02 from '../../assets/images/construcoes/B02.jpg';
+import B03 from '../../assets/images/construcoes/B03.jpg';
+import B04 from '../../assets/images/construcoes/B04.jpg';
+import B05 from '../../assets/images/construcoes/B05.jpg';
+import B06 from '../../assets/images/construcoes/B06.jpg';
 
+import tommy from '../../assets/images/customizacoes/tommy.jpg';
+import budokkan from '../../assets/images/customizacoes/budokkan.jpg';
+import five from '../../assets/images/customizacoes/five.jpg';
+import supersonic from '../../assets/images/customizacoes/supersonic.jpg';
+import neon from '../../assets/images/customizacoes/neon.jpg';
+
+import M01 from '../../assets/images/manutencoes/01.jpg';
+import M02 from '../../assets/images/manutencoes/02.jpg';
+import M03 from '../../assets/images/manutencoes/03.jpg';
+import M04 from '../../assets/images/manutencoes/04.jpg';
+import M05 from '../../assets/images/manutencoes/05.jpg';
+import M06 from '../../assets/images/manutencoes/06.jpg';
+import M07 from '../../assets/images/manutencoes/07.jpg';
+import M08 from '../../assets/images/manutencoes/08.jpg';
+import M09 from '../../assets/images/manutencoes/09.jpg';
+import M10 from '../../assets/images/manutencoes/10.jpg';
+import M11 from '../../assets/images/manutencoes/11.jpg';
+import M12 from '../../assets/images/manutencoes/12.jpg';
 
 const imagens = [
-  {
-    id: 1,
-    src: `${BASE_CONSTRUCOES}B01.jpg`,
-    alt: 'Construção 01',
-    descricao: 'Corpo e braço em cedro rosa; Escala em imbuia; Captadores Seymour Duncan; Trastes Dunlop Jumbo'
-  },
-  {
-    id: 2,
-    src: `${BASE_CONSTRUCOES}B02.jpg`,
-    alt: 'Construção 02',
-    descricao: 'Corpo em marupá; braço em tauari; escala em pau ferro; captadores Malagoli',
-  },
-  {
-    id: 3,
-    src: `${BASE_CONSTRUCOES}B03.jpg`,
-    alt: 'Construção 03',
-    descricao: 'Bloco central em pinus; asas em cedro rosa; escala em imbuia; captador single',
-  },
-  {
-    id: 4,
-    src: `${BASE_CONSTRUCOES}B04.jpg`,
-    alt: 'Construção 04',
-    descricao: 'Corpo e braço em mogno; escala em ébano; captadores Seymour Duncan; trastes jumbo',
-  },
-  {
-    id: 5,
-    src: `${BASE_CONSTRUCOES}B05.jpg`,
-    alt: 'Construção 05',
-    descricao: 'Guitarra baiana 6 cordas; Corpo em cedro rosa; braço em mogno; escala em imbuia; captador GFS',
-  },
-  {
-    id: 6,
-    src: `${BASE_CONSTRUCOES}B06.jpg`,
-    alt: 'Construção 06',
-    descricao: 'Corpo em cedro e laterais em pinus; braço em grápia; escala em ipê; traste jumbo de inox; ponte e captacão Wilkinson',
-  },
-  {
-    id: 7,
-    src: `${BASE_CUSTOMIZACOES}tommy.jpg`,
-    alt: 'Custom TommyThayer',
-    descricao: 'Guitarra Dolphin foi customizada para ficar no estilo da Gibson Les Paul signature do Tommy Thayer (Kiss).',
-  },
-  {
-    id: 8,
-    src: `${BASE_CUSTOMIZACOES}budokkan.jpg`,
-    alt: 'Custom Buddokan',
-    descricao: 'Guitarra Dolphin foi customizada para ter aparência semelhante a Gibson Les Paul Budokkan do Ace Frehley (Kiss).',
-  },
-  {
-    id: 9,
-    src: `${BASE_CUSTOMIZACOES}five.jpg`,
-    alt: 'Custom Bass 5 Strings',
-    descricao: 'Baixo Dolphin foi feito um novo braço e customizado de 4 para 5 cordas, madeira do braço em imbuia.',
-  },
-  {
-    id: 10,
-    src: `${BASE_CUSTOMIZACOES}supersonic.jpg`,
-    alt: 'Custom Supersonic',
-    descricao: 'Guitarra Giannini Supersonic dos anos 60 foi recuperada e a pintura foi feita para se aproximar da pintura original da mesma a época.',
-  },
-  {
-    id: 11,
-    src: `${BASE_CUSTOMIZACOES}neon.jpg`,
-    alt: 'Custom Neon',
-    descricao: 'Escudo da guitarra foi customizado com material que brilha ao estar sob luz negra para ter um efeito neon no palco',
-  },
-  {
-    id: 12,
-    src: `${BASE_MANUTENCOES}01.jpg`,
-    alt: 'Manutenção 01',
-    descricao: '',
-  },
-  {
-    id: 13,
-    src: `${BASE_MANUTENCOES}02.jpg`,
-    alt: 'Manutenção 02',
-    descricao: '',
-  },
-  {
-    id: 14,
-    src: `${BASE_MANUTENCOES}03.jpg`,
-    alt: 'Manutenção 03',
-    descricao: '',
-  },
-  {
-    id: 15,
-    src: `${BASE_MANUTENCOES}04.jpg`,
-    alt: 'Manutenção 04',
-    descricao: '',
-  },
-  {
-    id: 16,
-    src: `${BASE_MANUTENCOES}05.jpg`,
-    alt: 'Manutenção 05',
-    descricao: '',
-  },
-  {
-    id: 17,
-    src: `${BASE_MANUTENCOES}06.jpg`,
-    alt: 'Manutenção 06',
-    descricao: '',
-  },
-  {
-    id: 18,
-    src: `${BASE_MANUTENCOES}07.jpg`,
-    alt: 'Manutenção 07',
-    descricao: '',
-  },
-  {
-    id: 19,
-    src: `${BASE_MANUTENCOES}08.jpg`,
-    alt: 'Manutenção 08',
-    descricao: '',
-  },
-  {
-    id: 20,
-    src: `${BASE_MANUTENCOES}09.jpg`,
-    alt: 'Manutenção 09',
-    descricao: '',
-  },
-  {
-    id: 21,
-    src: `${BASE_MANUTENCOES}10.jpg`,
-    alt: 'Manutenção 10',
-    descricao: '',
-  },
-  {
-    id: 22,
-    src: `${BASE_MANUTENCOES}11.jpg`,
-    alt: 'Manutenção 11',
-    descricao: '',
-  },
-  {
-    id: 23,
-    src: `${BASE_MANUTENCOES}12.jpg`,
-    alt: 'Manutenção 12',
-    descricao: '',
-  },
+  { id: 1, src: B01, alt: 'Construção 01', descricao: 'Corpo e braço em cedro rosa; Escala em imbuia; Captadores Seymour Duncan; Trastes Dunlop Jumbo' },
+  { id: 2, src: B02, alt: 'Construção 02', descricao: 'Corpo em marupá; braço em tauari; escala em pau ferro; captadores Malagoli' },
+  { id: 3, src: B03, alt: 'Construção 03', descricao: 'Bloco central em pinus; asas em cedro rosa; escala em imbuia; captador single' },
+  { id: 4, src: B04, alt: 'Construção 04', descricao: 'Corpo e braço em mogno; escala em ébano; captadores Seymour Duncan; trastes jumbo' },
+  { id: 5, src: B05, alt: 'Construção 05', descricao: 'Guitarra baiana 6 cordas; Corpo em cedro rosa; braço em mogno; escala em imbuia; captador GFS' },
+  { id: 6, src: B06, alt: 'Construção 06', descricao: 'Corpo em cedro e laterais em pinus; braço em grápia; escala em ipê; traste jumbo de inox; ponte e captacão Wilkinson' },
+  { id: 7, src: tommy, alt: 'Custom TommyThayer', descricao: 'Guitarra Dolphin foi customizada para ficar no estilo da Gibson Les Paul signature do Tommy Thayer (Kiss).' },
+  { id: 8, src: budokkan, alt: 'Custom Buddokan', descricao: 'Guitarra Dolphin foi customizada para ter aparência semelhante a Gibson Les Paul Budokkan do Ace Frehley (Kiss).' },
+  { id: 9, src: five, alt: 'Custom Bass 5 Strings', descricao: 'Baixo Dolphin foi feito um novo braço e customizado de 4 para 5 cordas, madeira do braço em imbuia.' },
+  { id: 10, src: supersonic, alt: 'Custom Supersonic', descricao: 'Guitarra Giannini Supersonic dos anos 60 foi recuperada e a pintura foi feita para se aproximar da pintura original da mesma a época.' },
+  { id: 11, src: neon, alt: 'Custom Neon', descricao: 'Escudo da guitarra foi customizado com material que brilha ao estar sob luz negra para ter um efeito neon no palco' },
+  { id: 12, src: M01, alt: 'Manutenção 01', descricao: '' },
+  { id: 13, src: M02, alt: 'Manutenção 02', descricao: '' },
+  { id: 14, src: M03, alt: 'Manutenção 03', descricao: '' },
+  { id: 15, src: M04, alt: 'Manutenção 04', descricao: '' },
+  { id: 16, src: M05, alt: 'Manutenção 05', descricao: '' },
+  { id: 17, src: M06, alt: 'Manutenção 06', descricao: '' },
+  { id: 18, src: M07, alt: 'Manutenção 07', descricao: '' },
+  { id: 19, src: M08, alt: 'Manutenção 08', descricao: '' },
+  { id: 20, src: M09, alt: 'Manutenção 09', descricao: '' },
+  { id: 21, src: M10, alt: 'Manutenção 10', descricao: '' },
+  { id: 22, src: M11, alt: 'Manutenção 11', descricao: '' },
+  { id: 23, src: M12, alt: 'Manutenção 12', descricao: '' },
 ];
 
 export default imagens;
