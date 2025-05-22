43279c927229b2d2a01898607ae277d6564ddc74
GleberC <gleber10@yahoo.com.br>
2025-05-22 09:41:04 -0300

    publicação no vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cagnoni-guitars-react/', // ← EXATAMENTE como o nome do repo
   plugins: [react()],
 });
